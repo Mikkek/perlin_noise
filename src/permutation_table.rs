6590a903c11db11914e9b1f6e557f5af79a11746
Mikkek <54848327+Mikkek@users.noreply.github.com>
2022-10-10 17:49:19 +0200

    don't know if this works, but i'm not working on it more today >:(

--- src/permutation_table.rs
+++ src/permutation_table.rs
@@ -8,19 +8,41 @@ use rand_xoshiro::SplitMix64;
 
 const TABLE_SIZE: usize = 256;
 
+/// A pseudo-random permutation of the numbers [0; 256[
 pub struct PermutationTable {
     // Maybe just use a Vec<u8>?
     values: [u8; TABLE_SIZE],
 }
 
 impl PermutationTable {
+
+    /// Generates a new, random, PermutationTable from the given seed.
     pub fn new(seed: u64) -> Self {
         let mut prng = SplitMix64::seed_from_u64(seed);
         prng.gen()
     }
+
+    /// Hash into the PermutationTable with an arbitrary amount of points.
+    /// 
+    /// This is a simple Pearson Hash.
+    /// 
+    /// # Panics
+    /// 
+    /// If i made a very big mistake
+    pub fn hash(&self, input: &[isize]) -> u8 {
+        let index = input
+            .iter()
+            .map(|&a| (a & 0xff) as usize)  // Reduce values to the last 8 bits
+            .reduce(|a, b| self.values[a ^ b] as usize)
+            .unwrap();
+        self.values[index]
+    }
 }
 
-/// Define how a standart distribution should produce a PermutationTable
+/// Define how a standard distribution should produce a PermutationTable.
+/// 
+/// This means that anything implementing the [Rng] trait can produce a randomized PermutationTable, 
+/// but since a PermutationTable mut be created though the new() method, we only use [SplitMix64].
 impl Distribution<PermutationTable> for Standard {
     fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> PermutationTable {
         // The sorted table
